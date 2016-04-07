f8608e81e192bb84fe8f1671ebb718ec4da12c51
1102137203 <1102137203@gm.kuas.edu.tw>
2016-04-07 16:31:50 +0800

    0407-修改已傳入資料

--- HW/Controllers/searchController.cs
+++ HW/Controllers/searchController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -54,5 +55,111 @@ namespace HW.Controllers
             ViewBag.bingo = bingo;
             return View();
         }
+
+        //[HttpPost]
+        //public ActionResult search(FormCollection update)
+        //{
+        //    String orderId = update["orderId"];
+
+        //    return RedirectToAction("update",
+        //        new { orderId});
+        //}
+
+        public ActionResult update(String orderId)
+        {
+            //List<型態>
+            List<Orders> bingo = db.Orders.Where(x => x.OrderID.ToString() == orderId).ToList();//OrderID存到一個List
+
+            Orders data = new Orders();
+            String CustomersCompanyName="";
+            String EmployeesFirstName = "";
+            String ShippersCompanyName = "";
+
+            String orderDate = "";
+            String orderMonth = "";
+            String orderDay = "";
+
+            String requireDate = "";
+            String requireMonth = "";
+            String requireDay = "";
+
+            String shipDate = "";
+            String shipMonth = "";
+            String shipDay = "";
+
+
+            foreach (var item in bingo) //item是一個小變數，bingo是list，有list就要用foreach
+            {
+                data.OrderID = item.OrderID;
+                data.Freight = item.Freight;
+                data.ShipCountry = item.ShipCountry;
+                data.ShipCity = item.ShipCity;
+                data.ShipRegion = item.ShipRegion;
+                data.ShipPostalCode = item.ShipPostalCode;
+                data.ShipAddress = item.ShipAddress;
+                data.ShipName = item.ShipName;
+
+                CustomersCompanyName = item.Customers.CompanyName;
+                EmployeesFirstName = item.Employees.FirstName;
+                ShippersCompanyName = item.Shippers.CompanyName;
+
+                //日期，alterDate是function傳到下面
+                //orderDate
+                orderMonth =alterDate(item.OrderDate.Month.ToString());
+                orderDay =alterDate(item.OrderDate.Day.ToString());
+                orderDate = String.Format("{0}-{1}-{2}", item.OrderDate.Year, orderMonth, orderDay);
+
+                //requireDate
+                requireMonth = alterDate(item.RequiredDate.Month.ToString());
+                requireDay =alterDate( item.RequiredDate.Day.ToString());
+                requireDate= String.Format("{0}-{1}-{2}", item.RequiredDate.Year, requireMonth, requireDay);
+
+                //ShippedDate
+                shipMonth = alterDate(Convert.ToDateTime(item.ShippedDate).Month.ToString());
+                shipDay = alterDate(Convert.ToDateTime(item.ShippedDate).Day.ToString());
+                shipDate = String.Format("{0}-{1}-{2}", Convert.ToDateTime(item.ShippedDate).Year, shipMonth, shipDay);
+            }
+
+            ViewBag.data = data;
+            ViewBag.CustCompanyName = CustomersCompanyName;
+            ViewBag.EmployeesFirstName = EmployeesFirstName;
+            ViewBag.ShippersCompanyName = ShippersCompanyName;
+
+            ViewBag.orderDate = orderDate;
+            ViewBag.requireDate = requireDate;
+            ViewBag.shipDate = shipDate;
+
+            //客戶名稱
+            List<String> bingocomName = db.Customers.Where(x=>x.CompanyName!= CustomersCompanyName).Select(x=>x.CompanyName).ToList();//找全部Customers
+            ViewBag.custcompanyNameList = bingocomName;
+
+            //負責員工名稱
+            List<String> bingofirstName = db.Employees.Where(x => x.FirstName != EmployeesFirstName).Select(x => x.FirstName).ToList();
+            ViewBag.firstNameList = bingofirstName;
+
+            //出貨公司名稱
+            List<String> bingocompanyName = db.Shippers.Where(x => x.CompanyName != ShippersCompanyName).Select(x => x.CompanyName).ToList();
+            ViewBag.shipcompanyNameList = bingocompanyName;
+
+            
+
+            return View();
+        }
+
+        //我是日期的function
+        public String alterDate(String date) {
+            StringBuilder change = new StringBuilder();
+            if (date.Length < 2) {
+                change.Append("0");//append字串相加
+            }
+            change.Append(date);
+            return change.ToString();
+        }
+
+        public ActionResult add()
+        {
+            return View();
+        }
+
     }
 }
